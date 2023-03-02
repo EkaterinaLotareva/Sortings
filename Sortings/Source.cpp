6b037eeb0c1a320219ee1597b7163b41c80ecb9c
Kate Lotareva <k.v.lotareva@gmail.com>
2023-03-02 16:26:51 +0300

    вспомогательные функции

--- Sortings/Source.cpp
+++ Sortings/Source.cpp
@@ -4,6 +4,15 @@
 #include <fstream>
 using namespace std;
 
+int* random(int n) {
+	srand(200);
+	int* arr = new int[n];
+	for (int i = 0; i < n; i++) {
+		arr[i] = rand();
+	};
+	return arr;
+}
+
 bool forward_step(int array[], int min, int max) {
 	int t;
 	for (int i = min; i < max; i++) {
@@ -43,4 +52,46 @@ int *shaker(int array[], int n) {
 		min += 1;
 	}
 	return array;
+}
+
+bool testing_forward_step() {
+	for (int i = 10; i <= 100; i += 10) {
+		auto arr = random(i);
+		forward_step(arr, 0, i - 1);
+		for (int j = 0; j < i-1; j++) {
+			if (arr[j] > arr[i - 1]) { return false; }
+		}
+	}
+	return true;
+}
+
+bool testing_backward_step() {
+	for (int i = 10; i <= 100; i += 10) {
+		auto arr = random(i);
+		backward_step(arr, 0, i - 1);
+		for (int j = 1; j < i; j++) {
+			if (arr[j] <  arr[i - 1]) { return false; }
+		}
+	}
+	return true;
+}
+
+bool testing_shaker() {
+	for (int i = 10; i <= 100; i += 10) {
+
+	}
+}
+
+void writing_to_file(string s) {
+	std::ofstream ofs;
+	ofs.open("C:\\c_projects\\Sortings\\data.txt", std::ofstream::out | std::ofstream::app);
+
+	ofs << s << endl;
+
+	ofs.close();
+}
+
+int main() {
+
+	return 0;
 }
