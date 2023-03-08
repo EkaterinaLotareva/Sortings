c44c909c9dfca03ef96d15ac366378f6632ca51c
Kate Lotareva <k.v.lotareva@gmail.com>
2023-03-08 20:09:19 +0300

    последовательности хиббарда и фибоначи

--- Sortings/Source.cpp
+++ Sortings/Source.cpp
@@ -3,7 +3,8 @@
 #include <random>
 #include <fstream>
 using namespace std;
-
+int const N = 100000;
+int ARRAY[N];
 
 int* random(int n) {
 	srand(200);
@@ -99,12 +100,12 @@ bool testing_shaker() {
 	return true;
 }
 
-bool long_step(int array[], int n, int i) {
-	for (int j = 0; i + j < n; j += i) {
-		if (array[j] > array[j + i]) {
+bool bubble_step(int array[], int n, int step) {
+	for (int j = 0; step + j < n; j += step) {
+		if (array[j] > array[j + step]) {
 			auto t = array[j];
-			array[j] = array[j + i];
-			array[j + i] = t;
+			array[j] = array[j + step];
+			array[j + step] = t;
 			return false;
 		}
 	}
@@ -115,11 +116,59 @@ int* comb_sort(int array[], int n) {
 	bool sorted = false;
 	while (!sorted) {
 		for (int i = n - 1; i > 1; i--) {
-			long_step(array, n, i);
-			sorted = long_step(array, n, i);
+			bubble_step(array, n, i);
+			sorted = bubble_step(array, n, i);
 		}
 	}
 	return array;
+}
+
+void sort_step(int array[], int n, int step) {
+	for (int sorted = n - 1; sorted > 0; sorted-=step) {
+		for (int i = sorted - step; i < n - 1; i+=step) {
+			if (array[i] >= array[i + step]) {
+				auto t = array[i];
+				array[i] = array[i + step];
+				array[i = step] = t;
+			}
+		}
+	}
+}
+
+int* shell_sort_2(int array[], int n) {
+	auto step = n/2;
+	while (step >= 1) {
+		sort_step(array, n, step);
+		step = step / 2;
+	}
+}
+
+int fibonacci_sequence(int max) {
+	ARRAY[0] = 1;
+	ARRAY[1] = 1;
+	int n = 1;
+	for (int i = 0; ARRAY[i] + ARRAY[i + 1] <= max; i++) {
+		ARRAY[i + 2] = ARRAY[i] + ARRAY[i + 1];
+		n += 1;
+	}
+	return n;
+}
+
+int hibbard_sequence(int max) {
+	
+	return n;
+}
+
+int* shell_sort_hibbard(int array[], int n) {
+	auto step = n / 2;
+	while (step >= 1) {
+		sort_step(array, n, step);
+		step = step / 2;
+	}
+}
+
+int* shell_sort_fibonacci(int array[], int n) {
+
 }
 
 void writing_to_file(string s) {
@@ -133,17 +182,25 @@ void writing_to_file(string s) {
 
 void separate_atring() {
 	std::ofstream ofs;
-	ofs.open("C:\\c_projects\\Asymptotic complexity of algorithms\\data.txt", std::ofstream::out | std::ofstream::app);
+	ofs.open("C:\\c_projects\\Sortings\\data.txt", std::ofstream::out | std::ofstream::app);
 
 	ofs << endl;
 
 	ofs.close();
 }
 
+void time_comb_sort() {
+
+}
+
+void transpositions_comb_sort() {
+
+}
+
+
+
 int main() {
-	cout << testing_forward_step() << testing_backward_step() << testing_shaker() << endl;
-	int a[] = {3, 2, 1 };
-	array_output(comb_sort(a, 3), 3);
+	cout << testing_forward_step() << testing_backward_step() << testing_shaker();
 
 	return 0;
 }
